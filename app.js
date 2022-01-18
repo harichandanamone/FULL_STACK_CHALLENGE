64576bbc18170f0bf02178396d8882a1c8614a79
harichandanamone <harichandanamone@gmail.com>
2022-01-18 13:34:27 -0600

    Added node_modules

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.get('/',(req,res)=>{
+  res.send("Hello world");
+})
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
